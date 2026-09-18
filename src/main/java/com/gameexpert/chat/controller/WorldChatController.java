1411f2b3ba68b5d6483b488199400fd58923981f
JB1112 <lyo7256@naver.com>
2026-09-18 23:01:19 +0900

    Lv 6. 최근 채팅 조회 API 구현
    상태코드 200이니 ok 그대로 사용, 주소 매핑 및 주소에서 id를 받아올 수 있게 @PathVariable 세팅 @RequestParam(defaultValue = "50")로 default값 설정

--- src/main/java/com/gameexpert/chat/controller/WorldChatController.java
+++ src/main/java/com/gameexpert/chat/controller/WorldChatController.java
@@ -2,11 +2,11 @@ package com.gameexpert.chat.controller;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+import lombok.Builder;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.gameexpert.chat.dto.ChatMessageResponse;
 import com.gameexpert.chat.service.RecentChatQueryService;
@@ -19,8 +19,9 @@ public class WorldChatController {
 
     private final RecentChatQueryService chatService;
 
-    // TODO Lv 6: API 명세에 맞는 요청 매핑과 응답을 구현합니다.
-    public ResponseEntity<List<ChatMessageResponse>> chats(Long worldId, int limit) {
-        return ResponseEntity.ok(List.of());
+    @GetMapping("/worlds/{worldId}/chats")
+    public ResponseEntity<List<ChatMessageResponse>> chats(@PathVariable Long worldId, @RequestParam(defaultValue = "50") int limit) {
+
+        return ResponseEntity.ok(chatService.getRecentMessages(worldId,limit));
     }
 }
